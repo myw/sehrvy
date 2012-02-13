b6731ce4e4a733dbae9aa424ad2aca653b48bc11
Misha Wolfson <mywolfson@gmail.com>
2012-02-13 12:34:34 -0500

    Trivial connection test

---
+++ data/src/test_db.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+# Trivial script only intented to test connection. If you have a username and password,
+# run this script as follows:
+#
+#    DBI_USER=<your_username> DBI_PASS=<your_password> ./test_db.pl
+#
+# If no error message is printed, you're golden.
+
+use strict;
+use warnings;
+use DBI;
+
+
+MAIN: {
+	my $dbh = DBI->connect("dbi:mysql:sehrvy") or die "Cannot connect: $DBI::errstr";
+
+	$dbh->disconnect;
+}
