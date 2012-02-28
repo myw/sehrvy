d378267195aec26211636a099d36ce9ca32336f4
Misha Wolfson <mywolfson@gmail.com>
2012-02-28 01:36:07 -0500

    Add vendor name slugs to db

--- data/src/data_reader.pl
+++ data/src/data_reader.pl
@@ -4,6 +4,7 @@ use warnings;
 use DBI;
 use 5.008;
 
+
 MAIN: {
   my $dbh = DBI->connect('dbi:mysql:sehrvy','sehrvy_admin') or die "Cannot connect: $DBI::errstr";
 
@@ -41,8 +42,10 @@ sub create_tables {
 
   $dbh->do("CREATE TABLE Vendors(
            vendor_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+           vendor_slug VARCHAR(100) NOT NULL,
            vendor_name VARCHAR(100) NOT NULL,
-           UNIQUE KEY no_duplicate_vendors (vendor_name)
+           UNIQUE KEY no_duplicate_vendors (vendor_name),
+           UNIQUE KEY no_duplicate_vendor_slugs (vendor_slug)
            )") or die "Cannot create table: " . $dbh->errstr ();
 
   $dbh->do("CREATE TABLE Products(
@@ -170,8 +173,8 @@ sub read_state_codes {
 }
 
 # Read EHR data into the database
-sub read_data
-{
+sub read_data {
+
   my $dbh = shift;
   my %cached_vendors;
   my %cached_products;
@@ -182,14 +185,14 @@ sub read_data
   my $file = "../raw/ehr_data_sanitized.txt";
   open(FILE,$file) or die "Couldn't open $file";
 
-  my $get_vendor = $dbh->prepare("SELECT vendor_id FROM Vendors WHERE vendor_name=?") or die "Cannot prepare: " . $dbh->errstr ();
-  my $add_vendor = $dbh->prepare("INSERT INTO Vendors (vendor_name) VALUES (?)");
+  my $get_vendor = $dbh->prepare("SELECT vendor_id FROM Vendors WHERE vendor_slug =? AND vendor_name = ?") or die "Cannot prepare: " . $dbh->errstr ();
+  my $add_vendor = $dbh->prepare("INSERT INTO Vendors (vendor_slug, vendor_name) VALUES (?,?)");
 
   my $get_product = $dbh->prepare("SELECT product_id FROM Products WHERE vendor_id = ? AND product_name = ?") or die "Cannot prepare: " . $dbh->errstr ();
-  my $add_product = $dbh->prepare("INSERT INTO Products (vendor_id , product_name) VALUES (?,?)");
+  my $add_product = $dbh->prepare("INSERT INTO Products (vendor_id, product_name) VALUES (?,?)");
 
   my $get_version = $dbh->prepare("SELECT version_id FROM Versions WHERE product_id = ? AND version_name = ?") or die "Cannot prepare: " . $dbh->errstr ();
-  my $add_version = $dbh->prepare("INSERT INTO Versions (product_id , version_name) VALUES (?,?)");
+  my $add_version = $dbh->prepare("INSERT INTO Versions (product_id, version_name) VALUES (?,?)");
 
   my $get_provider_specialty = $dbh->prepare("SELECT provider_specialty_id FROM ProviderSpecialties WHERE provider_specialty_name = ?") or die "Cannot prepare: " . $dbh->errstr ();
   my $add_provider_specialty = $dbh->prepare("INSERT INTO ProviderSpecialties (provider_specialty_name) VALUES (?)");
@@ -205,6 +208,7 @@ sub read_data
     # Get vendor ID (adding vendor to Vendor table if necessary)
     my @handles = ($get_vendor,$add_vendor);
     my @args = (shift @tokenized_line);
+    unshift @args, slugify($args[0]); # slugify vendor name and put it in front
     my $vendor_id = add_if_necessary(\@handles,\@args,\%cached_vendors);
 
     # Get product ID (adding product to Product table if necessary)
@@ -313,5 +317,45 @@ sub add_if_necessary {
 
     return $row[0];
 }
+
+our %slugs;
+
+sub slugify {
+  my $string = shift;
+
+  my $slug = lc $string;
+
+  # Remove parentheticals
+  $slug =~ s/\(.*\)//g;
+
+  # Remove spaced-out dashes
+  $slug =~ s/ - /-/g;
+
+  # Kill anything that we can't easily parse
+  $slug =~ s/[^a-z -]//g;
+
+  # Kill inc, llc or pc tags
+  $slug =~ s/\b(llc|inc)\b//g;
+  $slug =~ s/\b(ltd|pc)$//g;
+
+  # Kill trailing or starting spaces
+  $slug =~ s/^\s+|\s+$//;
+
+  # Kill terminal buzzwords
+  $slug =~ s/\s+(corporation|industries|systems|services|healthcare|usa)$//g;
+  
+  # Convert legit spaces to dashes
+  $slug =~ s/ /-/g;
+
+  $slugs{$slug}{$string} = 1;
+
+  return $slug;
+}
+
+while (my ($key, $value) = each %slugs) {
+  my @strings = join ', ', keys %$value;
+  print "$key => @strings\n";
+}
+
 
 # vi:sw=2 ts=2 sts=2 et:
